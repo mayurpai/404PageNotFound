44297fcd4e0bc797566b97fe954d26e1a31a7eff
Mayur Pai B H <mayur5pai@gmail.com>
2020-08-21 20:31:37 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
 import crow from './Scarecrow.png';
+import bg from './bg.jpg';
+import bubble from './bubble.png';
 import './App.css';
 
 class App extends Component {
     render() {
         return <div className = "mainDiv" >
-            <
-            header > 404 Not Found < /header> <
-        img src = { crow }
-        alt = "Scarecrow" / > <
-            div className = "content" >
-            <
-            h1 > I Have A Bad News For You! < /h1> <
-        p > The Page You Are Looking For Might Be Removed Or Is Temporarily Unavailable. < /p> < /
-        div > < button > < /button> <
-        footer > 2020 < /footer > < /
-        div >
+        <header > 404 Not Found </header>
+        <img id="crow" src = { crow } alt = "Scarecrow" / >
+        <img id="bg" src = { bg } alt = "BG" / >
+        <img id="bubble" src = { bubble } alt = "Bubble" / >
+        <div className = "rotate" > </div>
+        <div className = "content" >
+                    <h1>
+                    <ul>
+                        <li>I</li>
+                        <li>&nbsp;</li>
+                        <li>H</li>
+                        <li>a</li>
+                        <li>v</li>
+                        <li>e</li>
+                        <li>&nbsp;</li>
+                        <li>A</li>
+                        <li>&nbsp;</li>
+                        <li>B</li>
+                        <li>a</li>
+                        <li>d</li>
+                        <li>&nbsp;</li>
+                        <li>N</li>
+                        <li>e</li>
+                        <li>w</li>
+                        <li>s</li>
+                        <li>&nbsp;</li>
+                        <li>F</li>
+                        <li>o</li>
+                        <li>r</li>
+                        <li>&nbsp;</li>
+                        <br/>
+                        <li>Y</li>
+                        <li>o</li>
+                        <li>u</li>
+                        <li>!</li>
+                        <li>&nbsp;</li>
+                    </ul>
+                    </h1>
+             <p > The Page You Are Looking For Might Be Removed Or Is Temporarily Unavailable. < /p> 
+             </div > < button id = "one" > Back To Home Page </button> 
+             <footer >Handcrafted with Love by Mayur Pai</footer > 
+             </div >
     }
 }
 
